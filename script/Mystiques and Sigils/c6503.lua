9e342f2032737dd2f9f71903d4e7ee722623e196
Nerfvous <137426719+Nerfvous@users.noreply.github.com>
2023-07-29 14:44:55 +0800

    Updated Hinoko, The Mystique
    1. Updated Hinoko's script to a better format.
    2. Fixed an issue where she can activate her 2nd effect without a cost.

--- script/Mystiques and Sigils/c6503.lua
+++ script/Mystiques and Sigils/c6503.lua
@@ -26,7 +26,6 @@ function s.initial_effect(c)
     e3:SetRange(LOCATION_MZONE)
     e3:SetCountLimit(1)
     e3:SetCost(s.xdcost)
-    e3:SetCondition(s.xdcon)
     e3:SetTarget(s.xdt)
     e3:SetOperation(s.xdop)
     c:RegisterEffect(e3)
@@ -55,47 +54,48 @@ end
 function s.xdcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         local ct=0
-        if Duel.IsPlayerCanSendtoGrave(tp)
+        if not (Duel.IsPlayerCanSendtoGrave(tp)
         and Duel.IsExistingMatchingCard(s.costfil,tp,LOCATION_HAND+LOCATION_SZONE,0,1,nil)
-        then ct=ct+1 end
-        if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCanBeEffectTarget),tp,0,LOCATION_MZONE,1,nil)
+        and Duel.IsExistingMatchingCard(s.xdfil,tp,LOCATION_DECK,0,1,nil))
+        then return false else ct=ct+1 end
+        if Duel.IsExistingTarget(aux.FaceupFilter(Card.IsMonster),tp,0,LOCATION_MZONE,1,e:GetHandler())
         then ct=ct+1 end
         e:SetLabel(ct)
         return ct~=0
     end
+    local ct=e:GetLabel()
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
     local g=Duel.SelectMatchingCard(tp,s.costfil,tp,LOCATION_HAND+LOCATION_SZONE,0,1,ct,nil)
     e:SetLabel(#g)
     Duel.SendtoGrave(g,REASON_COST)
 end
-function s.xdcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsPlayerCanSendtoGrave(tp)
-end
 function s.xdfil(c,e)
     return c:IsSetCard(0x28a) or c:IsSetCard(0x28c) and c:IsAbleToGrave()
 end
 function s.xdt(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.xdfil,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(e:GetLabel())
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,e:GetLabel())
-    Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_MZONE)
+    if chk==0 then return true end
+    local ct=e:GetLabel()
+    if ct>=1 then
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK) end
+    if ct>=2 then
+    e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_MZONE) end
 end
 function s.xdop(e,tp,eg,ep,ev,re,r,rp)
-    local p,lb=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+    local lb=e:GetLabel()
     local g=Duel.GetMatchingGroup(s.xdfil,tp,LOCATION_DECK,0,nil)
+    --Send 1 "Mystique" card or 1 "Sigil" Spell/Trap
     if lb>=1 and #g>0 then
-        --Send 1 "Mystique" card or 1 "Sigil" Spell/Trap
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
         local sc=g:Select(tp,1,1,nil)
         Duel.SendtoGrave(sc,REASON_EFFECT)
     end
     --Target and destroy
     if lb>=2
-    and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsCanBeEffectTarget),tp,0,LOCATION_MZONE,1,nil) then
+    and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsMonster),tp,0,LOCATION_MZONE,1,e:GetHandler()) then
         Duel.BreakEffect()
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-        local sc=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsCanBeEffectTarget),tp,0,LOCATION_MZONE,1,1,nil)
+        local sc=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsMonster),tp,0,LOCATION_MZONE,1,1,e:GetHandler())
         Duel.Destroy(sc,REASON_EFFECT)
     end
 end
